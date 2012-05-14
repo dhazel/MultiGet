92663c4d46b700a9d3808c05ca0e7fe0a4c32117
David Hazel <dhazel@gmail.com>
2012-05-14 09:10:34 -0700

    Added docblocks, more type-hinting and type-checking, modified bracing.

--- MultiGet.php
+++ MultiGet.php
@@ -1,43 +1,100 @@
 <?php
-/*
-
-$mget = new MultiGet(3);
-$url = '...';
-$userVar = '...';
-$mget->request($url, $postFields, $curlOptions)
-->on('success', function ($content, $url, $handle) use ($userVar) {
-
-})
-->on('error', function ($url, $handle) {
-
-})
-->on('complete', function ($content, $url, $handle) {
-
-});
-
-$mget.go();
-
-*/
+/**
+ * @file
+ * Part of a refactoring of https://github.com/Yaffle/MultiGet.
+ *
+ */
 
 
-class MultiGet {
-
+/**
+ * $mget = new MultiGet(3);
+ * $url = '...';
+ * $userVar = '...';
+ * $mget->request($url, $postFields, $curlOptions)
+ * ->on('success', function ($content, $url, $handle) use ($userVar) {
+ * 
+ * })
+ * ->on('error', function ($url, $handle) {
+ * 
+ * })
+ * ->on('complete', function ($content, $url, $handle) {
+ * 
+ * });
+ * 
+ * $mget.go();
+ **/
+class MultiGet 
+{
+    /**
+     * The maximum number of requests to process simultaneously
+     * @var int
+     **/
     public $maxRequests;
+
+    /**
+     * @var array
+     * @see curl_setopt()
+     **/
     public $curlOptions;
 
+    /**
+     * @var array
+     **/
     private $requests;
-    private $queue = array();// order of loading is not garanted
-    
 
+    /**
+     * Used when count(requests) > maxRequests. 
+     * Note: order of loading is not guaranteed.
+     * @var array
+     **/
+    private $queue = array();
+
+    /**
+     * @var array
+     **/
     private $timeouts = array();
 
-    public function __construct($maxRequests = 4, $curlOptions = null) {
+    /**
+     * "success" event
+     **/
+    const SUCCESS = 'success';
+
+    /**
+     * "error" event
+     **/
+    const ERROR = 'error';
+
+    /**
+     * "complete" event
+     **/
+    const COMPLETE = 'complete';
+
+    /**
+     * Used for type-checking
+     * @var array
+     **/
+    public static $EVENTTYPES = array(
+        'SUCCESS' = self::SUCCESS,
+        'ERROR' => self::ERROR,
+        'COMPLETE' => self::COMPLETE,
+    );
+
+    /**
+     * @param int $maxRequests
+     * @param array $curlOptions
+     **/
+    public function __construct($maxRequests = 4, $curlOptions = null) 
+    {
         $this->maxRequests = $maxRequests;
         $this->requests = array();
         $this->curlOptions = $curlOptions;
     }
 
-    private function _request($x) {
+    /**
+     * @param MultiGet_Request $x
+     **/
+    private function _request(MultiGet_Request $x) 
+    {
         if (count($this->requests) < $this->maxRequests) {
             $multiHandle = curl_multi_init();
             $handle = curl_init();
@@ -69,7 +126,13 @@ class MultiGet {
         }
     }
 
-    public function request($url, $postFields = null, $curlOptions = null) {
+    /**
+     * @param string $url
+     * @param array|string $postFields
+     * @param array $curlOptions
+     **/
+    public function request($url, $postFields = null, $curlOptions = null) 
+    {
         $postData = null;
         if (isset($postFields) && is_array($postFields)) {
             $postData = '';
@@ -85,7 +148,15 @@ class MultiGet {
         return $x;
     }
     
-    public function setTimeout($callback, $delay) {
+    /**
+     * Timeouts are callable functions that will run prior to the processing
+     *  of requests.
+     * @param callable $callback
+     * @param int $delay
+     *  The delay period, in microseconds.
+     **/
+    public function setTimeout($callback, $delay) 
+    {
         $this->timeouts[] = array(
             'callback' => $callback,
             'delay' => $delay,
@@ -93,7 +164,13 @@ class MultiGet {
         );
     }
 
-    public function go() {
+    /**
+     * Processes the requests.
+     *
+     * When this completes, all requests will have been processed.
+     **/
+    public function go() 
+    {
         while (count($this->requests) > 0 || count($this->timeouts) > 0) {
         
         //echo count($this->requests) . ' / ' . count($this->timeouts) . " - r/t \n";
@@ -151,11 +228,11 @@ class MultiGet {
             }
 
             if (curl_error($completed->handle)) {
-                $completed->emit('error', curl_error($completed->handle), $completed->url, $completed->handle);
+                $completed->emit(self::ERROR, curl_error($completed->handle), $completed->url, $completed->handle);
             } else {
-                $completed->emit('success', $content, $completed->url, $completed->handle);          
+                $completed->emit(self::SUCCESS, $content, $completed->url, $completed->handle);          
             }
-            $completed->emit('complete', $content, $completed->url, $completed->handle, $responseHeaders);
+            $completed->emit(self::COMPLETE, $content, $completed->url, $completed->handle, $responseHeaders);
 
             curl_close($completed->handle);
         }
--- MultiGet/Request.php
+++ MultiGet/Request.php
@@ -1,28 +1,90 @@
 <?php
+/**
+ * @file
+ * Part of a refactoring of https://github.com/Yaffle/MultiGet.
+ *
+ */
 
 
-class MultiGet_Request {// similar to EventEmitter !?
 
+
+/**
+ * This may be similar to Node.js EventEmitter
+ *
+ **/
+class MultiGet_Request 
+{
+
+    /**
+     * @var string
+     **/
     public $url;
+
+    /**
+     * @var array|string
+     **/
     public $postData;
+
+    /**
+     * @var array
+     * @see curl_setopt()
+     **/
     public $curlOptions;
+
+    /**
+     * CURL handle
+     * @var resource
+     **/
     public $handle = null;
+
+    /**
+     * CURL multi handle
+     * @var resource
+     **/
     public $multiHandle = null;
 
-    public function __construct($url, $postData = null, $curlOptions = null) {
+    /**
+     * The array of callbacks
+     * @var array
+     **/
+    private $listeners = array();
+
+    /**
+     * @param string $url
+     * @param array|string $postData
+     * @param array $curlOptions
+     **/
+    public function __construct($url, $postData = null, $curlOptions = null) 
+    {
         $this->url = $url;
         $this->postData = $postData;
         $this->curlOptions = $curlOptions;
     }
 
-    private $listeners = array();
-
-    public function on($eventType, $callback) {
+    /**
+     * @param string $eventType
+     * @param callable $callback
+     **/
+    public function on($eventType, $callback) 
+    {
+        if ( ! in_array($eventType, MultiGet::EVENTTYPES) ) {
+            throw new Exception('The event type "'.$eventType.'" is not a valid event!');
+        }
         $this->listeners[] = array('eventType' => $eventType, 'callback' => $callback);
         return $this;
     }
 
-    public function emit($eventType/*, $arg1, $arg2, ... */) {
+    /**
+     * Emits events
+     * @param string $eventType
+     * @param mixed ...
+     *  Variable arguments supplied to the callback functions.
+     **/
+    public function emit($eventType/*, $arg1, $arg2, ... */) 
+    {
+        if ( ! in_array($eventType, MultiGet::EVENTTYPES) ) {
+            throw new Exception('The event type "'.$eventType.'" is not a valid event!');
+        }
         $args = array_slice(func_get_args(), 1);
         $candidates = array_slice($this->listeners, 0);
         for ($i = 0; $i < count($candidates); $i++) {
